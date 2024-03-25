7cbc68148ba90fe73025378ecbd24cfb6c7f5039
Damilola Adisa <damilolaadisa@Adisas-MacBook-Pro.local>
2024-03-25 19:22:03 +0100

    fixed the image

--- TastyTrails/src/components/cards/AllFood.tsx
+++ TastyTrails/src/components/cards/AllFood.tsx
@@ -1,70 +1,3 @@
-// import { Box, Button, Image, Text } from '@chakra-ui/react';
-// import React, { useState } from 'react';
-
-// interface AllFoodProps {
-//   imageUrl: string;
-//   name: string;
-// }
-
-// const AllFood: React.FC<AllFoodProps> = ({ imageUrl, name }) => {
-//   const [quantity, setQuantity] = useState<number>(0);
-
-//   const handleIncrement = () => {
-//     setQuantity(quantity + 1);
-//   };
-
-//   const handleDecrement = () => {
-//     if (quantity > 0) {
-//       setQuantity(quantity - 1);
-//     }
-//   };
-
-//   return (
-
-
-//     <Box display={["block", 'flex', 'block', 'flex']} borderTopWidth='1px' px='10px' borderTopColor='#f1f1f1' borderBottomWidth='1px' borderBottomColor='#f1f1f1'  py='20px' textAlign={['center','left', 'center','left']} alignItems="center" cursor='pointer' >
-//       <Box mr="4" shadow='base' boxSize={['200px','100px','150px', '100px']} rounded='md' overflow='hidden'  mx={['auto', '0', 'auto', '0']} >
-//         <Image src={imageUrl} alt="food" />
-//       </Box>
-//       <Box flex="1" mx='10px' >
-//         <Text fontSize={['md','lg', "xl"]} fontWeight="bold" mb="1" noOfLines={1}>
-//           {name}
-//         </Text>
-//         <Text color='gray.400' fontSize={['sm','md', "lg"]} noOfLines={2}>
-//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, modi?
-//         </Text>
-//       </Box>
-//       <Box display="flex" alignItems="center" mx='10px' my={['10px','0','10px','0']} justifyContent={['center','flex-start','center', 'flex-start']}>
-//         <Button onClick={handleDecrement} fontSize="22px" shadow='base' rounded='md' px='8px' mr="2">
-//           -
-//         </Button>
-//         <Text>{quantity}</Text>
-//         <Button onClick={handleIncrement} fontSize="22px" ml="2" shadow='base' rounded='md' px='8px'>
-//           +
-//         </Button>
-//       </Box>
-//       <Button mt={['10px', '0','10px','0']} px='15px' rounded='lg' py='5px' borderColor='orange.500' borderWidth='2px' textAlign={['center']} mx='auto'>Add</Button>
-//     </Box>
-
-//   );
-// };
-
-// export default AllFood;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 import { Box, Button, Image, Text } from '@chakra-ui/react';
@@ -74,9 +7,10 @@ interface AllFoodProps {
   id: string;
   imageUrl: string;
   name: string;
+  price: number;
 }
 
-const AllFood: React.FC<AllFoodProps> = ({ id, imageUrl, name }) => {
+const AllFood: React.FC<AllFoodProps> = ({ id, imageUrl, name,price }) => {
   const [quantity, setQuantity] = useState<number>(0);
 
   const handleIncrement = () => {
@@ -117,6 +51,7 @@ const AllFood: React.FC<AllFoodProps> = ({ id, imageUrl, name }) => {
         <Text color='gray.400' fontSize={['sm','md', "lg"]} noOfLines={2}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, modi?
         </Text>
+        <Text className="">$ {price} </Text>
       </Box>
       <Box display="flex" alignItems="center" mx='10px' my={['10px','0','10px','0']} justifyContent={['center','flex-start','center', 'flex-start']}>
         <Button onClick={handleDecrement} fontSize="22px" shadow='base' rounded='md' px='8px' mr="2">
--- TastyTrails/src/components/swipersjs/MenuSwippers.tsx
+++ TastyTrails/src/components/swipersjs/MenuSwippers.tsx
@@ -1,157 +1,7 @@
-// import React, { useState, useEffect } from 'react';
-// import { Box, Text, Spinner } from '@chakra-ui/react';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/css';
-// import 'swiper/css/pagination';
-// import '../css/MenuSwipper.css';
-// import { AllFood, MenuCard, MenuNavbar } from '..';
-
-// interface Category {
-//   idCategory: string;
-//   strCategory: string;
-// }
-
-// interface Meal {
-//   idMeal: string;
-//   strMeal: string;
-//   strMealThumb: string;
-// }
-
-// const MenuSwippers: React.FC = () => {
-//   const [categories, setCategories] = useState<Category[]>([]);
-//   const [meals, setMeals] = useState<Meal[]>([]);
-//   const [selectedCategory, setSelectedCategory] = useState<string>('Vegan');
-//   const [loading, setLoading] = useState<boolean>(true);
-//   const [error, setError] = useState<string | null>(null);
-//   const baseUrl: string = 'https://www.themealdb.com/api/json/v1/1/';
-
-//   useEffect(() => {
-//     const fetchCategories = async () => {
-//       try {
-//         const response = await fetch(`${baseUrl}categories.php`);
-//         if (!response.ok) {
-//           throw new Error('Failed to fetch categories');
-//         }
-//         const data = await response.json();
-//         if (!data.categories) {
-//           throw new Error('No categories found');
-//         }
-//         setCategories(data.categories);
-//         fetchMealsByCategory(selectedCategory);
-//       } catch (error) {
-//         setError(error.message);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchCategories();
-//   }, [selectedCategory]);
-
-//   const fetchMealsByCategory = async (category: string) => {
-//     setLoading(true); // Set loading to true when fetching meals
-//     try {
-//       const response = await fetch(`${baseUrl}filter.php?c=${category}`);
-//       if (!response.ok) {
-//         throw new Error('Failed to fetch meals');
-//       }
-//       const data = await response.json();
-//       if (!data.meals) {
-//         throw new Error('No meals found');
-//       }
-//       setMeals(data.meals);
-//     } catch (error) {
-//       setError(error.message);
-//     } finally {
-//       setLoading(false); // Set loading to false after fetching meals
-//     }
-//   };
-
-
-
-//   if (loading) {
-//     return (
-//       <Box display="flex" zIndex='99999' justifyContent="center" alignItems="center" height="100vh">
-//         <Spinner size="xl" color="green.500" />
-//       </Box>
-//     );
-//   }
-
-//   if (error) {
-//     return <div>Error: {error}</div>;
-//   }
-
-//   return (
-
-//     <>
-//       <Box display="" maxW="1340px" mx="auto">
-//         <Box display="grid" gap='20px' gridTemplateColumns={{ base: "100%", md: "45% 55%" }}  gridTemplateRows="auto auto">
-//           <MenuNavbar />
-//           <Swiper
-//             slidesPerView={5}
-//             spaceBetween={10}
-//             pagination={{
-//               clickable: true,
-//             }}
-//             className="mySwiper"
-//           >
-//             {categories.map((category) => (
-//               <SwiperSlide key={category.idCategory} onClick={() => setSelectedCategory(category.strCategory)}>
-//                 <MenuCard categoryName={category.strCategory} />
-//               </SwiperSlide>
-//             ))}
-//           </Swiper>
-
-//           <Box bg="transparent" gridColumn="2 / span 1" gridRow="1 / span 3">
-         
-//           </Box>
-
-//           <Box gridColumn="1 / span 1" overflowY="auto" maxHeight="calc(100vh - 250px)">
-//             <Text>All Food</Text>
-//             {!loading && (
-//               <Box minWidth="100%" display='grid' gap='25px'>
-//                 {meals.map((meal) => (
-//                  <div key={meal.idMeal} >
-//                  <AllFood imageUrl={meal.strMealThumb} name={meal.strMeal}  />
-//                </div>
-               
-//                 ))}
-//               </Box>
-//             )}
-//           </Box>
-//         </Box>
-//       </Box>
-//     </>
-//   );
-// };
-
-// export default MenuSwippers;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 import React, { useState, useEffect } from 'react';
-import { Box, Text, Spinner } from '@chakra-ui/react';
+import { Box, Text, Spinner ,Image} from '@chakra-ui/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -191,7 +41,8 @@ const MenuSwippers: React.FC = () => {
         }
         setCategories(data.categories);
         fetchMealsByCategory(selectedCategory);
-      } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error:any) {
         setError(error.message);
       } finally {
         setLoading(false);
@@ -200,6 +51,13 @@ const MenuSwippers: React.FC = () => {
 
     fetchCategories();
   }, [selectedCategory]);
+
+  useEffect(() => {
+    // Set the image of the first meal when the component mounts
+    if (meals.length > 0) {
+      setSelectedMealImage(meals[0].strMealThumb);
+    }
+  }, [meals]);
 
   const fetchMealsByCategory = async (category: string) => {
     setLoading(true);
@@ -213,7 +71,8 @@ const MenuSwippers: React.FC = () => {
         throw new Error('No meals found');
       }
       setMeals(data.meals);
-    } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error:any) {
       setError(error.message);
     } finally {
       setLoading(false);
@@ -224,10 +83,14 @@ const MenuSwippers: React.FC = () => {
     setSelectedMealImage(imageUrl);
   };
 
+  function generateRandomPrice(min: number, max: number) {
+    const price = (Math.random() * (max - min) + min).toFixed(2);
+    return parseFloat(price);
+  }
   if (loading) {
     return (
       <Box display="flex" zIndex='99999' justifyContent="center" alignItems="center" height="100vh">
-        <Spinner size="xl" color="green.500" />
+        <Spinner size="5xl" color="green.500" />
       </Box>
     );
   }
@@ -257,7 +120,7 @@ const MenuSwippers: React.FC = () => {
           </Swiper>
 
           <Box bg="transparent" gridColumn="2 / span 1" gridRow="1 / span 3">
-            {selectedMealImage && <img src={selectedMealImage} alt="Selected Meal" />}
+          {selectedMealImage && <Image w='full' objectFit='cover'  h='full' src={selectedMealImage} alt="Selected Meal" />}
           </Box>
 
           <Box gridColumn="1 / span 1" overflowY="auto" maxHeight="calc(100vh - 250px)">
@@ -266,7 +129,7 @@ const MenuSwippers: React.FC = () => {
               <Box minWidth="100%" display='grid' gap='25px'>
                 {meals.map((meal) => (
                   <div key={meal.idMeal} onClick={() => handleMealClick(meal.strMealThumb)}> {/* Pass the meal image URL to the handler */}
-                    <AllFood imageUrl={meal.strMealThumb} name={meal.strMeal} />
+                   <AllFood imageUrl={meal.strMealThumb} name={meal.strMeal} id={meal.idMeal}   price={generateRandomPrice(10, 20)}/>
                   </div>
                 ))}
               </Box>
